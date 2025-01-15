926773508247c6c0e67dafb3f51a56b6401d5cb0
SWE-Surya <141406782+SWE-Surya@users.noreply.github.com>
2025-01-15 17:16:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
-# Databricks notebook source
-from setuptools import find_packages, setup
+
+from setuptools import setup, find_packages
 
 setup(
-    name='Remote_DWH_Accelarator',
-    version='0.1',
-    packages=find_packages()
+    name="Remote_DWH_Accelarator",  # Name of the package
+    version="0.1",  # Initial version
+    packages=find_packages(),  # Automatically find packages inside your project
+    description="Utilities for CK DWH Accelerator",  # A short description of your project
+    author="Surya",  # Your name
+    
+    url="https://github.com/SWE-Surya/Remote_DWH_Accelarator",  # Repository URL
+    classifiers=[
+        "Programming Language :: Python :: 3",  # Compatible Python version
+        "License :: OSI Approved :: MIT License",  # Your project license
+        "Operating System :: OS Independent",  # Compatibility
+    ],
+    python_requires='>=3.6',  # Minimum Python version required
+
 )
