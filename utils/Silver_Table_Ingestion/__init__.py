9e88e3ffc02cdf30bd0e147d6592728c45e8241c
SWE-Surya <141406782+SWE-Surya@users.noreply.github.com>
2025-01-15 18:37:38 +0530

    Update __init__.py

--- utils/Silver_Table_Ingestion/__init__.py
+++ utils/Silver_Table_Ingestion/__init__.py
@@ -11,7 +11,7 @@
      },
      "inputWidgets": {},
      "nuid": "5bd22dc9-0d6d-461f-aa3a-fb00070ea127",
-     "showTitle": 0,
+     "showTitle": False,
      "tableResultSettingsMap": {},
      "title": ""
     }
